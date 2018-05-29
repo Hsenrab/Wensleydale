9c6a62046413ac3802031ad79e6fe909ef05710d
Hsenrab <Hsenrab@users.noreply.github.com>
2018-05-29 13:56:25 +0100

    Added AllOn for LEDs when being fitted.

---
+++ LightsProgram/Scripts/AllOn.py
@@ -0,0 +1,18 @@
+ #!/usr/bin/env python3
+import ScriptSetup
+"""Sample script to put all lights on."""
+#import HardwareControl.Lights.Virtual.wlight
+import Internals.Lights.colourschemes as colorschemes
+
+NUM_LED = 12*144
+
+# All on as white light - to stop Ctrl C
+print('All On')
+
+while True:
+  MY_CYCLE = colorschemes.Solid(num_led=NUM_LED, pause_value=1,
+                             num_steps_per_cycle=1000, num_cycles=1, global_brightness=5)
+                             
+  MY_CYCLE.start()
+
+print('Finished the test')
