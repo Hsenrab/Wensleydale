1f761187c65491625f945d4cc2e54a423296ba2e
Lucy-sb <lucylulumay@gmail.com>
2018-06-06 14:43:54 +0000

    logging added to random routine

--- EyeProgram/Scripts/InfiniteScripts/Gromit_Random_Routine.py
+++ EyeProgram/Scripts/InfiniteScripts/Gromit_Random_Routine.py
@@ -19,8 +19,15 @@ try:
     while(True):
         wlogger.log_info("Begin Cycle")
         for i in range(random.randint(3, 8)):
-            myController.straight_to_point(myController.speeds[random.randint(0,4)], myController.positions[random.randint(0,9)])
+            speed = myController.speeds[random.randint(0,4)]
+            position = myController.positions[random.randint(0,9)]
+            
+            wlogger.log_info("beginning move: " + str(position) + " at speed: " + str(speed))
+            
+            myController.straight_to_point(speed, position)
             myController.randomSleep()
+            
+        wlogger.log_info("beginning eye roll")
         myController.Gromit_Eye_Roll()
         myController.randomSleep()
         
