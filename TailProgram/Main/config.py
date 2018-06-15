90e3b0e58e04ae043d02e53ecc65f5ca2e85e2b9
HSenrab <hllewoh@gmail.com>
2018-06-15 15:10:54 +0000

    Fixed random wag and removed logging print

--- TailProgram/Main/config.py
+++ TailProgram/Main/config.py
@@ -22,7 +22,7 @@ button_press_count = 0
 touchInputPin = 15
 touchOutputPin = 5
 
-
-config.num_cycles_before_random_wag = 300
-config.random_wag_frequency = 300
-config.random_wag_length = 100
+# Random wag constants
+num_cycles_before_random_wag = 300
+random_wag_frequency = 300
+random_wag_length = 100
